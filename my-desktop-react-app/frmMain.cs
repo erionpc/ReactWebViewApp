2284e8acf25af3fdfad1c6874b79b86239875098
erionpc <erionpc@yahoo.it>
2021-08-04 00:34:46 +0100

    created desktop app with React in webview

---
+++ my-desktop-react-app/frmMain.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }    
+    }
+}
